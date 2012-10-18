61cc748f0e6eba778833f62c2a88e69a46c13b68
Jason Eastwood <jeastwood@gmail.com>
2012-10-18 09:43:58 -0500

    Added Role GET

--- lib/ibssm_api_client/role.rb
+++ lib/ibssm_api_client/role.rb
@@ -9,6 +9,19 @@ module IbssmApiClient
       response      
     end
     
+    def roles(token,user_id)
+      coeus_roles = Array.new
+      path = "roles/show"
+      response = get_data(token,path,'?coeus_user_id=' + user_id.to_s)
+      if role_data = response["roles"]
+        role_data.each do |map|
+          coeus_role = coeus_role_for_ibssm_role(map['name'])
+          coeus_roles << coeus_role if !coeus_role.blank?
+        end
+      end
+      coeus_roles.to_json
+    end
+    
     def build_role_json(roles)
       bucket = Array.new
       roles.each do |role|
@@ -51,6 +64,34 @@ module IbssmApiClient
       }
       
       dictionary[role_name]
+    end
+    
+    def coeus_role_for_ibssm_role(role_name)
+      dictionary = {
+        'g-office_staff' => 'office_staff',
+        'g-intern' => 'intern', 
+        'g-staff' => 'staff',
+        'g-student' => 'student',
+        'g-administrator' => 'admin',
+        'g-superuser' => 'root',
+        'g-approve_first_year' => '1st_year_approver',
+        'authenticate_attendance_stations' => 'authenticate_attendance_stations',
+        'interview_first_year_students' => '1st_year_interviewer',
+        'interview_2nd_year_students' => '2nd_year_interviewer', 
+        'view_applications' => 'application_viewer',
+        'travel_admin' => 'travel_admin',
+        'travel_manager' => 'travel_manager',
+        'travel_leader' => 'travel_leader',
+        'travel_finance_admin' => 'travel_finance_admin',
+        'view_travel' => 'travel_viewer',
+        'api_admin' => 'api_admin',
+        'accept_2nd_year_students' => '2nd_year_approver',
+        'missions_staff' => 'missions_staff',
+        'missions_finance_admin' => 'missions_finance_admin',
+        'missions_admin' => 'missions_admin'
+      }
+      
+      dictionary[role_name]      
     end
     
   end
