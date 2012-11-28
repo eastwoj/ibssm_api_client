3cf7e3cd6573a6ad3c5a32628c18de4569c4ddf3
Jason Eastwood <jeastwood@gmail.com>
2012-11-28 14:31:27 -0500

    Removed translate role logic.
    The client should only know about symfony roles as it will potentially
    be used by multiple applications.

--- lib/ibssm_api_client/role.rb
+++ lib/ibssm_api_client/role.rb
@@ -1,101 +1,42 @@
 module IbssmApiClient
   class Role < Base
 
-    def update_all_roles(token,user)
+    # updates a user by clearing his roles and assigning the symfony roles passed
+    def update_all_roles(token,external_user_id,symfony_roles)
       path = "roles/update"
-      payload = build_role_json(user.roles)
-      puts "payload: #{payload}"
-      response = post_data(token,path,{'coeus_user_id' => user.id, 'roles' => payload})
+      response = post_data(token,path,{'coeus_user_id' => external_user_id, 'roles' => symfony_roles.to_json})
       debug_response(response)
       response      
     end
     
+    # adds a role by symfony role name
+    def add_role(token,external_user_id,symfony_role_name)
+      path = "roles/add"
+      response = post_data(token,path,{'coeus_user_id' => external_user_id, 'role' => symfony_role_name})
+      debug_response(response)
+      response
+    end
+    
+    # removes the symfony role by name
+    def remove_role(token,external_user_id,symfony_role_name)
+      path = "roles/remove"
+      response = post_data(token,path,{'coeus_user_id' => external_user_id, 'role' => symfony_role_name})
+      debug_response(response)
+      response
+    end
+    
+    # returns a list of symfony roles for this user
     def roles(token,user_id)
-      coeus_roles = Array.new
+      roles = Array.new
       path = "roles/show"
       response = get_data(token,path,'?coeus_user_id=' + user_id.to_s)
       if role_data = response["roles"]
-        role_data.each do |map|
-          coeus_role = coeus_role_for_ibssm_role(map['name'])
-          coeus_roles << coeus_role if !coeus_role.blank?
+        role_data.each do |role|
+          roles << role['name']
         end
       end
-      coeus_roles.to_json
+      roles.to_json
     end
-    
-    def build_role_json(roles)
-      bucket = Array.new
-      roles.each do |role|
-        bucket << ibssm_role_for_coeus_role(role.name)
-      end
-      bucket.to_json
-    end
-    
-    def build_role_json_with_names(role_names)
-      bucket = Array.new
-      role_names.each do |role_name|
-        bucket << ibssm_role_for_coeus_role(role_name)
-      end
-      bucket.to_json
-    end
-    
-    def ibssm_role_for_coeus_role(role_name)
-      dictionary = {
-        'office_staff' => 'g-office staff',
-        'intern' => 'g-intern', 
-        'staff' => 'staff',
-        'staff_pastor' => 'g-staff',
-        'student' => 'g-student',
-        'admin' => 'g-administrator',
-        'root' => 'g-superuser',
-        '1st_year_approver' => 'g-approve_first_year',
-        'authenticate_attendance_stations' => 'Authenticate Attendance Stations',
-        '1st_year_interviewer' => 'first_year_interview',
-        '2nd_year_interviewer' => 'interview_2nd_year_students', 
-        'application_viewer' => 'view_applications',
-        'travel_admin' => 'travel_admin',
-        'travel_manager' => 'travel_manager',
-        'travel_leader' => 'travel leader',
-        'travel_finance_admin' => 'travel finance admin',
-        'travel_viewer' => 'view_travel',
-        'api_admin' => 'api_admin',
-        '2nd_year_approver' => 'accept_2nd_year_students',
-        'missions_staff' => 'missions_staff',
-        'missions_finance_admin' => 'missions_finance_admin',
-        'missions_admin' => 'missions_admin'
-      }
-      
-      dictionary[role_name]
-    end
-    
-    def coeus_role_for_ibssm_role(role_name)
-      dictionary = {
-        'g-office staff' => 'office_staff',
-        'g-intern' => 'intern', 
-        'g-staff' => 'staff_pastor',
-        'g-student' => 'student',
-        'g-administrator' => 'admin',
-        'g-superuser' => 'root',
-        'g-approve_first_year' => '1st_year_approver',
-        'staff' => 'staff',
-        'Authenticate Attendance Stations' => 'authenticate_attendance_stations',
-        'first_year_interview' => '1st_year_interviewer',
-        'interview_2nd_year_students' => '2nd_year_interviewer', 
-        'view_applications' => 'application_viewer',
-        'travel_admin' => 'travel_admin',
-        'travel_manager' => 'travel_manager',
-        'travel leader' => 'travel_leader',
-        'travel finance admin' => 'travel_finance_admin',
-        'view_travel' => 'travel_viewer',
-        'api_admin' => 'api_admin',
-        'accept_2nd_year_students' => '2nd_year_approver',
-        'missions_staff' => 'missions_staff',
-        'missions_finance_admin' => 'missions_finance_admin',
-        'missions_admin' => 'missions_admin'
-      }
-      
-      dictionary[role_name]      
-    end
-    
   end
 end
+
